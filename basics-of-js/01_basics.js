413d4f23d8c564b484e506e2bb97026bfc1150bb
vikasofficial100 <147232756+vikasofficial100@users.noreply.github.com>
2023-10-07 14:12:05 +0000

    datatypes

--- basics-of-js/01_basics.js
+++ basics-of-js/01_basics.js
@@ -2,4 +2,5 @@ console.log("first-js-program")
 const a=25
 let b="vikas"
 var c="not use this"  //do not use this methode because some issue in block scope & functional scope.
-console.table([a, b, c])
+var d; //write ";" or not ,there is no compulsory
+console.table([a, b, c ,d])
