d5008b03c755fc1541707fd3d3041be49eefb3bb
vikasofficial100 <147232756+vikasofficial100@users.noreply.github.com>
2023-10-18 06:20:15 +0000

    operator_precedence

--- basics-of-js/03_conversion_and _operations.js
+++ basics-of-js/03_conversion_and _operations.js
@@ -35,35 +35,35 @@ let value = 3
 let negValue = -value
 // console.log(negValue);
 
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(2**3);
-// console.log(2/3);
-// console.log(2%3);
-
+ console.log(2+2);
+ console.log(2-2);
+ console.log(2*2);
+ console.log(2**3);   //it means 2 to power 3
+ console.log(2/3);
+ console.log(2%3);  //use to get reminder (modulus sign)
+ //---------TRICKY OPERATIONS BASED ON OPERRATOR PRECIDENCE----------
 let str1 = "hello"
 let str2 = " hitesh"
 
-let str3 = str1 + str2
+let str3 = str1 + str2   //we can add string like this but not subtracte.
 // console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
-
+// console.log("1" + 2);    //here js engine will automatically covert 2 into string OUTPUT 12
+// console.log(1 + "2");   //here js engine will automatically covert 1 into string OUTPUT 12
+// console.log("1" + 2 + 2); //here js engine will automatically covert 2 ,2 into string OUTPUT 122
+// console.log(1 + 2 + "2"); //but here whole game changed firstly 1+2 hua then string"2" printed as it is OUTPUT 32
 // console.log( (3 + 4) * 5 % 3);
 
-// console.log(+true);
-// console.log(+"");
+// console.log(+true); //OUTPUT 1
+// console.log(+""); //OUTPUT 0
 
 let num1, num2, num3
 
 num1 = num2 = num3 = 2 + 2
 
 let gameCounter = 100
-++gameCounter;
+++gameCounter;   //-----> preincrement
+gameCounter++;   //----->postincrement
 console.log(gameCounter);
 
 // link to study
